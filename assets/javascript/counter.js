d4936d0362f911cd4485496d39d6a4ab93e46b6a
mberthia <mberthiaume00@gmail.com>
2020-09-10 16:37:02 -0400

    first commit

---
+++ assets/javascript/counter.js
@@ -0,0 +1,25 @@
+// The best-practice is to pull all the elements into variables, in order to avoid
+// searching the HTML tree for these elements more than once:
+let incrementButton = document.querySelector("#increment");
+let decrementButton = document.querySelector("#decrement");
+let counter = document.querySelector("#counter");
+
+incrementButton.addEventListener("click", function () {
+  console.log("+ button clicked");
+  let newCounterValue = Number(counter.innerHTML) + 1;
+  if (newCounterValue >= 10) {
+    counter.style.color = "red";
+  }
+  counter.innerHTML = newCounterValue;
+});
+
+decrementButton.addEventListener("click", function () {
+  console.log("- button clicked");
+  let newCounterValue = Number(counter.innerHTML) - 1;
+  if (newCounterValue < 10) {
+    counter.style.color = "black";
+  }
+  if (counter.innerHTML > 0) {
+    counter.innerHTML = newCounterValue;
+  }
+});
